5565bbdc42346d2e06e9265b8a5fb017f894aaa7
niloy32073 <mdnrahmanx27@gmail.com>
2025-05-02 01:30:54 +0600

    Done

--- src/main/kotlin/repositories/MessageRepository.kt
+++ src/main/kotlin/repositories/MessageRepository.kt
@@ -29,6 +29,7 @@ class MessageRepository: MessageRepositoryInterface {
         val senderIds = MessageTable.select(MessageTable.senderId).where{ MessageTable.receiverId eq userId }.map{it[MessageTable.senderId]}
         val receiverIds = MessageTable.select(MessageTable.receiverId).where{ MessageTable.senderId eq userId }.map{it[MessageTable.receiverId]}
         val connectedUserIds =(senderIds + receiverIds).filter { it != userId }.toSet()
+        println(connectedUserIds)
         UserTable.selectAll().where{ UserTable.id inList connectedUserIds.toList() }.map {
             println(it)
             UserBasicInfo(userId = it[UserTable.id], name = it[UserTable.name])
